ddd3e400d1d8eb771cf414566e93c23bf4ea8d2b
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-06-15 15:47:44 -0300

    build: start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import { BooksList } from "./components/BooksList/BooksList";
+import { CreateBook } from "./components/CreateBook/CreateBook";
+import { UpdateBook } from "./components/UpdateBook/UpdateBook";
+import { NavBar } from "./components/shared/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+        <Routes>
+          <Route path="/update-book" element={<UpdateBook />} />
+          <Route path="/create-book" element={<BooksList />} />
+          <Route path="/" element={<CreateBook />} />
+        </Routes>
+    </>
   );
 }
 
